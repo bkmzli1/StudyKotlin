52d36a1c8de8c32c44174607e42e6caab883710b
makromant <gradientretro@gmail.com>
2017-09-25 06:34:45 +0300

    complete lesson2.task1 and lesson2.task2

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -1,4 +1,5 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task1
 
 import lesson1.task1.discriminant
@@ -33,7 +34,18 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    when {
+        age % 100 in 5..20 ->
+            return "$age лет"
+        age % 10 == 1 ->
+            return "$age год"
+        age % 10 in 2..4 ->
+            return "$age года"
+        else ->
+            return "$age лет"
+    }
+}
 
 /**
  * Простая
@@ -44,7 +56,15 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double {
+    val half_way = ((v1 * t1) + (v2 * t2) + (v3 * t3)) / 2
+    if (v1 * t1 >= half_way)
+        return half_way / v1
+    else if (v1 * t1 + v2 * t2 >= half_way)
+        return t1 + (half_way - v1 * t1) / v2
+    else
+        return t1 + t2 + (half_way - v1 * t1 - v2 * t2) / v3
+}
 
 /**
  * Простая
@@ -57,7 +77,21 @@ fun timeForHalfWay(t1: Double, v1: Double,
  */
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
-                       rookX2: Int, rookY2: Int): Int = TODO()
+                       rookX2: Int, rookY2: Int): Int {
+    var wins = 0
+    if (kingY == rookY1 || kingX == rookX1)
+        wins++
+    if (kingY == rookY2 || kingX == rookX2)
+        wins++
+    if (wins == 0)
+        return 0
+    else if (wins == 2)
+        return 3
+    else if (wins == 1 && (kingY == rookY2 || kingX == rookX2))
+        return 2
+    else
+        return 1
+}
 
 /**
  * Простая
@@ -71,7 +105,21 @@ fun whichRookThreatens(kingX: Int, kingY: Int,
  */
 fun rookOrBishopThreatens(kingX: Int, kingY: Int,
                           rookX: Int, rookY: Int,
-                          bishopX: Int, bishopY: Int): Int = TODO()
+                          bishopX: Int, bishopY: Int): Int {
+    var wins = 0
+    if (kingX == rookX || kingY == rookY)
+        wins++
+    if ((bishopX - bishopY == kingX - kingY) || (bishopX + bishopY == kingX + kingY))
+        wins++
+    if (wins == 0)
+        return 0
+    else if (wins == 2)
+        return 3
+    else if (wins == 1 && kingX == rookX || kingY == rookY)
+        return 1
+    else
+        return 2
+}
 
 /**
  * Простая
@@ -81,7 +129,32 @@ fun rookOrBishopThreatens(kingX: Int, kingY: Int,
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    if ((a > b + c) || (b > a + c) || (c > b + a))
+        return -1
+    else if (a > b && a > c) {
+        if (a * a == b * b + c * c)
+            return 1
+        if (a * a < b * b + c * c)
+            return 0
+        else
+            return 2
+    } else if (b > a && b > c) {
+        if (b * b == a * a + c * c)
+            return 1
+        if (b * b < a * a + c * c)
+            return 0
+        else
+            return 2
+    } else {
+        if (c * c == b * b + a * a)
+            return 1
+        if (c * c < b * b + a * a)
+            return 0
+        else
+            return 2
+    }
+}
 
 /**
  * Средняя
@@ -91,4 +164,15 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
+    if (a > d || b < c)
+        return -1
+    else if (a <= d && d <= b && a <= c)
+        return d - c
+    else if (c <= b && b <= d && a < c)
+        return b - c
+    else if (c <= a && b <= d)
+        return b - a
+    else
+        return d - a
+}
